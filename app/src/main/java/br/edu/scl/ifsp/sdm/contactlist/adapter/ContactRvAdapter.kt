901d9de371b59a359150197a948ba9414170d8be
Yohel D. Larrauri Pizarro <yohel.larrpizarro@gmail.com>
2023-11-28 20:24:53 -0300

    Campo Telefone adicionado na celula

--- app/src/main/java/br/edu/scl/ifsp/sdm/contactlist/adapter/ContactRvAdapter.kt
+++ app/src/main/java/br/edu/scl/ifsp/sdm/contactlist/adapter/ContactRvAdapter.kt
@@ -19,6 +19,7 @@ class ContactRvAdapter(
         RecyclerView.ViewHolder(tileContactBinding.root){
         val nameTv: TextView = tileContactBinding.nameTv
         val emailTv: TextView = tileContactBinding.emailTv
+        val phoneTv: TextView = tileContactBinding.phoneTv
 
         init {
             tileContactBinding.root.apply {
@@ -55,6 +56,7 @@ class ContactRvAdapter(
             with(holder){
                 nameTv.text = contact.name
                 emailTv.text = contact.email
+                phoneTv.text = contact.phone
             }
         }
     }
